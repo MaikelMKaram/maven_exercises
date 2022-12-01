77aba84d3a5591c8cbb24ae8fb352e3e5814af0a
Maikel Karam <maikel.m.karam@gmail.com>
2022-12-01 15:43:03 -0600

    Dependency Management exercise complete

---
+++ src/main/java/whatever.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class whatever {
+    public static void main(String[] args) {
+
+        String something = "whatever";
+        System.out.println("StringUtils.isNumeric(something) = " + StringUtils.isNumeric(something));
+        System.out.println("StringUtils.swapCase(something) = " + StringUtils.swapCase(something));
+        System.out.println("StringUtils.reverse(something) = " + StringUtils.reverse(something));
+
+    }
+}
